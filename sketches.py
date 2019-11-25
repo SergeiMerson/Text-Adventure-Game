c9853fb44312cdea7cbe09f415d6dfea01d2df73
SergeiMerson <sergei.merson@gmail.com>
2019-11-25 08:44:08 +0200

    minor changes

--- sketches.py
+++ sketches.py
@@ -1,29 +1,28 @@
-def chessboard(N: int, board:list=None, queen_positions:set =set()):
+def chessboard(N: int, board:list=None, queen_positions:list = []):
     # If no chessboard was passed create a new one
     if board is None:
-        board = {(x, y) for x in range(N) for y in range(N)}
+        board = [(x, y) for x in range(N) for y in range(N)]
 
     # Remove all unsuitable cells:
     for x_queen, y_queen in queen_positions:
-        board = {(x,y) for x, y in board
-                 if (x != x_queen) and (y != y_queen) and (abs(y - y_queen) != abs(x - x_queen))}
+        board = [(x,y) for x, y in board
+                 if (x != x_queen) and (y != y_queen) and (abs(y - y_queen) != abs(x - x_queen))]
 
     if not board:
         return queen_positions
 
-    possible_solutions = []
+    solutions = []
 
     for cell in board:
         qp_copy = queen_positions.copy()
-        qp_copy.add(cell)
+        qp_copy.append(cell)
 
-        # iterate over possible solution:
-        possible_solutions.append(chessboard(N, board, qp_copy))
+        # get additional options:
+        solutions.append(chessboard(N, board, qp_copy))
 
-    # Remove duplicates:
-    solutions = []
-    [solutions.append(s) for s in ]
-    return possible_solutions
+    # remove duplicates:
+
+    return solutions
 
 
 print(chessboard(3))
